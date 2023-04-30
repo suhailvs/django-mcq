822ccc3b9a9dc56477ef80b53d8d12dfb3b4adab
suhailvs <suhailvs@gmail.com>
2023-04-30 17:28:18 +0530

    start app quiz

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # django-quiz
+
+
+Installation
+------------
+Install django-quiz
+
+Run `pip install django-quiz`.
+
+Add `'quiz'` to your `INSTALLED_APPS` setting.
+
+    INSTALLED_APPS = (
+        ...
+        'quiz',
+        ...
+    )
+
+Add the following to your projects `urls.py` file.
+
+
+    urlpatterns = [
+        ...
+        path('quiz/', include('quiz.urls')),
+    ]
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from io import open
+
+readme = open('README.md', encoding='utf-8').read()
+
+setup(
+    name='django-quiz',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='A configurable quiz app for Django.',
+    long_description=readme,
+    url='https://github.com/suhailvs/django-quiz',
+    author='Suhail VS',
+    author_email='suhailvs@gmail.com',
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'Django>=4.0.0'
+    ]
+)
